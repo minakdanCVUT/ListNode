2b58194c461adf61383fb2ff2d09b6f2ff1af725
minakdan <132306332+minakdanCVUT@users.noreply.github.com>
2024-01-02 11:48:19 +0100

    Update removeDuplicates.c

--- removeDuplicates.c
+++ removeDuplicates.c
@@ -4,3 +4,20 @@ typedef struct SinglyListNode{
 }SinglyListNode;
 
 
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* llist) {
+    SinglyLinkedListNode * tmp = llist, * duplicate = NULL;
+    while(tmp){
+        if(tmp->next){
+            if(tmp->data == tmp->next->data){
+                duplicate = tmp->next;
+                tmp->next = duplicate->next;
+                free(duplicate);
+            }else{
+                tmp = tmp->next;
+            }
+        }else{
+            break;
+        }   
+    }
+    return llist;
+}
